1056fa5bb676f3cb3d4f829efecc709d9a353de9
Eri-Murakami <toypoodle.trumpet.35978@outlook.jp>
2024-04-21 23:01:35 +0900

    BuyItemconfirm

--- src/com/diworksdev/ecsite/action/BuyItemAction.java
+++ src/com/diworksdev/ecsite/action/BuyItemAction.java
@@ -8,20 +8,17 @@ import com.opensymphony.xwork2.ActionSupport;
 
 public class BuyItemAction extends ActionSupport implements SessionAware {
 	
-	private int stock;
-	private String pay;
 	public Map<String, Object> session;
-	private String result;
+	private int count;
+	private String pay;
 	
 	public String execute() {
-		result = SUCCESS;
-		
-		session.put("stock",stock);
-		int intStock = Integer.parseInt(session.get("stock").toString());
+		String result = SUCCESS;
+		session.put("count", count);
+		int intCount = Integer.parseInt(session.get("count").toString());
 		int intPrice = Integer.parseInt(session.get("buyItem_price").toString());
-		session.put("buyItem_price", intStock * intPrice);
+		session.put("total_price", intCount * intPrice);
 		String payment;
-		
 		if(pay.equals("1")) {
 			payment = "現金払い";
 			session.put("pay", payment);
@@ -30,23 +27,14 @@ public class BuyItemAction extends ActionSupport implements SessionAware {
 			session.put("pay", payment);
 		}
 		return result;
-	}
-	public int getStock() {
-		return stock;
-	}
-	
-	public void setStock(int stock) {
-		this.stock = stock;
-	}
-	public String getPay() {
-		return pay;
+		}
+	public void setCount(int count) {
+		this.count = count;
 	}
 	public void setPay(String pay) {
 		this.pay = pay;
 	}
-	public Map<String, Object> getSession(){
-		return session;
-	}
+	
 	@Override
 	public void setSession(Map<String, Object> session) {
 		this.session = session;
